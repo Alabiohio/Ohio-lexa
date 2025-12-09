bfe8b4daf24db47816ef234c388fbb8c3c0629de
Alabiohio <ohiocheoyaalabi@gmail.com>
2025-12-09 02:56:47 +0100

    updated model

--- src/assets/js/chat2.js
+++ src/assets/js/chat2.js
@@ -13,7 +13,7 @@ const { marked } = require('marked');
 
 
 const API_KEY = process.env.REACT_APP_GEMINI_API_KEY;
-const model = "gemini-2.0-flash";
+const model = "gemini-2.5-flash";
 export const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=` + API_KEY;
 const inputBox = document.getElementById("userInputBox");
 const recordSpan = document.getElementById("rcdSpan");
